a5cf81f5e9b70faf32e77a118d7c9cf63af39e8e
Jordan Snow <jordan@snowdev.co>
2024-04-30 16:32:53 -0600

    Initial Commit

---
+++ ServersFriend/ServersFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServersFriendApp.swift
+//  ServersFriend
+//
+//  Created by Jordan Snow on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ServersFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
