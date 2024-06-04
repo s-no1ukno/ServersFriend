27476fd701a8aaf62188546f524df75b1e0dcb99
Jordan Snow <jordan@snowdev.co>
2024-06-04 14:00:05 -0600

    feat: navigation for employer stack

--- ServersFriend/Nav Views/ShiftsView.swift
+++ ServersFriend/Nav Views/ShiftsView.swift
@@ -11,7 +11,6 @@ import SwiftData
 struct ShiftsView: View {
   
   @Environment(\.modelContext) var modelContext
-  @State private var path = [Shift]()
   @State private var navPath = NavigationPath()
 //  @Query var shifts: [Shift]
   @Query var employers: [Employer]
@@ -21,7 +20,7 @@ struct ShiftsView: View {
 //  let noNameMsg = Text("No name specified").font(.system(size: 12)).italic()
 
   var body: some View {
-    NavigationStack(path: $path) {
+    NavigationStack {
       VStack {
         Text("Keep track of all of your job data here. Drag an object to the left to delete, and to the right to edit.")
           .padding(20)
@@ -32,6 +31,7 @@ struct ShiftsView: View {
                 Text(emp.name)
               }
             }
+            .onDelete(perform: deleteEmployer)
           }
           Section("My Positions"){
             ForEach(positions) { pos in
@@ -39,6 +39,7 @@ struct ShiftsView: View {
                 Text(pos.name)
               }
             }
+            .onDelete(perform: deletePosition)
           }
           Section("My Shifts"){
             ForEach(shifts) { shift in
@@ -46,24 +47,20 @@ struct ShiftsView: View {
                 Text(shift.name)
               }
             }
+            .onDelete(perform: deleteShift)
           }
         }
-        //        ForEach(shifts) { shift in
-        //          NavigationLink(value: shift) {
-        //            Text(shift.name)
-        //          }
-        //        }
-        //        .onDelete(perform: deleteShift)
-        //      }
         .navigationTitle("Job Data")
+        .navigationDestination(for: Employer.self) { emp in
+          Text("Employer")
+          emp.name.isEmpty ? Text("No name") : Text(emp.name)
+        }
+        .navigationDestination(for: Position.self) { pos in
+          Text("Position")
+        }
         .navigationDestination(for: Shift.self) { shift in
           Text("Shift")
         }
-        //      .navigationDestination(for: Shift.self) { shift in
-        //        EditShiftView(shift: shift)
-        //      }
-        //      .toolbar {
-        //        Button("Add Shift", systemImage: "plus", action: addShift)
       }
     }
   }
@@ -80,6 +77,25 @@ struct ShiftsView: View {
 //      modelContext.delete(shift)
 //    }
 //  }
+  
+  func deleteEmployer(at offsets: IndexSet) {
+    for offset in offsets {
+      let target = employers[offset]
+      modelContext.delete(target)
+    }
+  }
+  func deletePosition(at offsets: IndexSet) {
+    for offset in offsets {
+      let target = positions[offset]
+      modelContext.delete(target)
+    }
+  }
+  func deleteShift(at offsets: IndexSet) {
+    for offset in offsets {
+      let target = shifts[offset]
+      modelContext.delete(target)
+    }
+  }
 }
 
 #Preview {
